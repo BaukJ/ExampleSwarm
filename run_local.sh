37780699bd60ac7cbba442f3c32605735b1427ee
Kuba Jasko <jakub.jasko@accenture.com>
2019-02-25 09:41:16 +0000

    Added ldap config and split up compose files

--- run_local.sh
+++ run_local.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env sh
 
-export VERSION=${VERSION:-latest}
-docker-compose build
+
+export VERSION=${VERSION:-`git describe --tags`}
+
+printf "
+    USING VERSION: $VERSION
+"
+
+docker-compose -f docker-compose.yaml -f docker-compose-build.yaml build
 docker stack deploy --compose-file docker-compose.yaml example
